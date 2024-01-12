1c815894e36b92cd6165198e729d61558fa406f8
Jawahar S <jawahar273@gmail.com>
2024-01-12 23:21:28 +0530

    display host name

--- server/src/main.rs
+++ server/src/main.rs
@@ -92,6 +92,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tokio::spawn(twiddle_service_status(health_reporter.clone()));
     
     let host: String = env::var("R_HOST")?;
+    println!("host name -{}-", host);
     let addr: SocketAddr = host.parse().unwrap();
 
     let svc = Service::default();
