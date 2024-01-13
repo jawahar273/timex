ab041a648227dcbc31864f89fc248d55b63bf9a6
Jawahar S <jawahar273@gmail.com>
2024-01-13 18:07:13 +0530

    feat: rewrites of api

--- server/web/next.config.js
+++ server/web/next.config.js
@@ -1,8 +1,18 @@
+const rewrites = () => {
+    return [
+        {
+            source: '/api/:path*',
+            destination: 'https://timex.up.railway.app/:path*'
+        }
+    ]
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
         typedRoutes: true,
-    }
+    },
+    rewrites: rewrites,
 };
 
 module.exports = nextConfig;
